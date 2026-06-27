bd40444d4d19212448b343bab5c2bd32a119e88a
HsrRaza <hr.raza04@gmail.com>
2026-06-27 21:52:04 +0530

    small user chat

--- chat/src/index.ts
+++ chat/src/index.ts
@@ -9,20 +9,62 @@ const openai = new OpenAI({
     apiKey: process.env.OPENROUTER_API_KEY
 })
 
-async function run() {
-    const input = promptSync({sigint:true}) 
-    const userInput = input("Enter your message ")
-    const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            { role: "user", content: userInput },
+type Context = {
+    role: 'user'| 'assistant' | 'system',
+    content: string
+}[]
 
-        ]
+const context: Context = [
+    {
+        role: "system",
+        content: "You are helpfull assistant"
+    },
+    {
+        role: "user",
+        content: "Hello how are you"
+    }
+];
+async function chatCompletion() {
+    const response = await openai.chat.completions.create({
+        messages: context,
+        model: "gpt-3.5-turbo",
+
+    })
+    const responseMessage = response.choices[0]?.message;
+    context.push({
+        role: "assistant",
+        content: responseMessage?.content
     })
 
-    console.log(response.choices[0]?.message.content);
-
+    console.log(`Assistant : ${response.choices[0]?.message.role}` , response.choices[0]?.message.content );
     
+}
+
+
+async function run() {
+    const input = promptSync({ sigint: true })
+
+    while (true) {
+        const userInput = input() as string
+
+        if (userInput.toLowerCase() === "exit") {
+            console.log("Exiting the chat");
+            break;
+        }
+
+
+        context.push({
+            role:"user",
+            content:userInput
+        })
+
+        await chatCompletion()
+
+    }
+
+
+
+
 
 }
 run()
