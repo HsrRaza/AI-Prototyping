a577b3039b1b373180bcecc017f79330cd8a44ee
HsrRaza <hr.raza04@gmail.com>
2026-07-03 20:57:31 +0530

    learn tool calling

--- tools/src/index.ts
+++ tools/src/index.ts
@@ -12,6 +12,9 @@ const openai = new OpenAI({
 
 
 
+function getTimeInNewYork(){
+    return new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+}
 
 async function callOpenAITools() {
 
@@ -24,11 +27,47 @@ async function callOpenAITools() {
     
     const response  = await openai.chat.completions.create({
         model:"gpt-3.5-turbo",
-        messages: context
-    })
+        messages: context,
+        tools:[
+            {
+                type:"function",
+                function:{
+
+                    name:"getTimeInNewYork",
+                    description:"Get current time in New York",
+                }
+            }
+        ],
+        tool_choice:'auto' // auto tool decision
+    });
+
+    // step 2 :decide the tool to use and call the tool
+    const willInvokeTheTool = response.choices[0]?.finish_reason ==="tool_calls";
+
+    const toolCall = response.choices[0]?.message.tool_calls?.[0];
+
+    if(willInvokeTheTool && toolCall && "function" in toolCall){
+        
+        const toolName  = toolCall.function.name;
+
+        if(toolName === "getTimeInNewYork"){
+            const toolResponse = getTimeInNewYork();
+            console.log("Tool Response: ", toolResponse);
+
+            context.push(response.choices[0]!.message);
+            context.push({role:"tool",  content:toolResponse , tool_call_id:toolCall?.id ?? " "});
+        }
+    }
+
+    const secondResponse  = await openai.chat.completions.create({
+        model:"gpt-3.5-turbo",
+        messages: context,
+    });
 
 
-    console.log(response.choices[0]?.message.content);
+
+
+    console.log(secondResponse.choices[0]?.message.content);
     
     
 }
